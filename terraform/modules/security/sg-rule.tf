77a4e83c80c1dd905919b044041d045ac1abc52d
Jay <Wiped@Jays-MacBook-Pro.local>
2023-06-09 22:21:38 -0400

    update security module with outbound all

--- terraform/modules/security/sg-rule.tf
+++ terraform/modules/security/sg-rule.tf
@@ -114,3 +114,12 @@ resource "aws_security_group_rule" "inbound-mysql-webserver" {
   source_security_group_id = aws_security_group.ACS["webserver-sg"].id
   security_group_id        = aws_security_group.ACS["datalayer-sg"].id
 }
+
+resource "aws_security_group_rule" "outbound_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ACS["webserver-sg"].id
+}
